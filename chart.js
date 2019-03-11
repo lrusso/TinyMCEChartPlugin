9baea55d2d02fa3caff6ee7638adc6f61525aee1
Leonardo Russo <contacto@lrusso.com.ar>
2019-03-11 10:29:34 -0300

    Add files via upload

--- chart.js
+++ chart.js
@@ -123,11 +123,33 @@ tinymce.PluginManager.add("chart", function(editor, url)
 				data: {labels:myLabels, datasets: myDatasets},
 				options:
 					{
+					showDatapoints: true,
 					responsive:false,
 					title: {display:false},
 					legend: {display:false},
 					scales: {xAxes:[{gridLines:{display:false},ticks:{min:0}}],yAxes:[{gridLines:{display:false},ticks:{min:0,precision:0}}]},
-					animation: {onComplete: chartToImg}
+					animation:
+						{
+						onComplete: function () 
+							{
+							var chartInstance = this.chart, ctx = chartInstance.ctx;
+							ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+							ctx.textAlign = "center";
+							ctx.textBaseline = "bottom";
+
+							this.data.datasets.forEach(function (dataset, i)
+								{
+								var meta = chartInstance.controller.getDatasetMeta(i);
+								meta.data.forEach(function (bar, index)
+									{
+									var data = dataset.data[index];
+									ctx.fillText(data, bar._model.x, bar._model.y - 5);
+									});
+								});
+
+							chartToImg();
+							}
+						},
 					}
 				});
 			}
